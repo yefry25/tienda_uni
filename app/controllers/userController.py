c249ad1db18b7eda45555322b10e834cc2c31e11
DESKTOP-MLL0337\jefferson <Jefferson Becerra>
2024-10-01 07:35:06 -0500

    implementación de metodo put, login de usuarios

--- app/controllers/userController.py
+++ app/controllers/userController.py
@@ -14,3 +14,24 @@ def get_users():
     from app.services.userService import get_users
     result = get_users()
     return jsonify(result)
+
+@bp.route('/updateUser/<int:id>', methods=["PUT"])
+def update_user(id):
+    from app.services.userService import update_user
+    data = request.json
+    result = update_user(id, data)
+    return jsonify(result)
+
+@bp.route('/login')
+def login():
+    from app.services.userService import login
+
+    # Recibe los datos de la solicitud (JSON) desde el cuerpo de la petición
+    data = request.json
+    
+    # Extrae username y password del JSON
+    username = data.get('username')
+    password = data.get('password')
+
+    result = login(username, password)
+    return jsonify(result)
--- app/models/user.py
+++ app/models/user.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from decimal import Decimal
 
 class User:
-    def __init__(self, id: int, nombres: str, apellidos: str, usuario: str, fecha_nacimiento: datetime, total_compras: Decimal, direccion: str, numero_telefonico: str):
+    def __init__(self, id: int, nombres: str, apellidos: str, usuario: str, fecha_nacimiento: datetime, total_compras: Decimal, direccion: str, numero_telefonico: str, password: str, estado: bool):
         self.id = id
         self.nombres = nombres
         self.apellidos = apellidos
@@ -11,3 +11,5 @@ class User:
         self.total_compras = total_compras
         self.direccion = direccion
         self.numero_telefonico = numero_telefonico
+        self.password = password
+        self.estado = estado
