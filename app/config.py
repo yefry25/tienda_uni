3d146a810b06d0cd5fa51c007aae408acba03155
DESKTOP-MLL0337\jefferson <Jefferson Becerra>
2024-10-15 09:10:36 -0500

    muchas cosas

--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    # Configuración de la base de datos MySQL
-    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
-    MYSQL_DATABASE = os.getenv('MYSQL_DATABASE', 'tienda')
-    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '')
